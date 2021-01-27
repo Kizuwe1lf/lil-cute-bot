f2d8e59ec6fb39255c8e6abf3b057fc827c5265d
Kizuwe1lf <Kizuwe1lf@gmail.com>
2021-01-27 22:41:09 +0300

    ntp fix

--- bot_commands/c_ntp.py
+++ bot_commands/c_ntp.py
@@ -1,57 +1,71 @@
 from bot_commands.c_main import stuff
 
+def get_list_of_pp(arr):
+    pp = []
+    for play in arr:
+        pp.append(float(play['pp']))
+    return pp
 
-def get_ntp(osu_user, given_tuple):
-    given_pp = []
-    for x in range(len(given_tuple)):
-        given_pp.append(float(given_tuple[x]))
+def calculate_total_pp(arr):
+    i = 0
+    total_pp = 0
+    while i < len(arr):
+        total_pp += arr[i] * (0.95 ** i)  # PP Weight Formula
+        i += 1
+    return total_pp
+
+def sort_pp_list(arr):
+    arr.sort(reverse = True)
+    return arr
+
+def get_ntps(pp_tuple, pp_list): # when len(given_tuple) > 1
+    given_pp_list = []
+    for pp_value in pp_tuple:
+        given_pp_list.append(float(pp_value))
+
+    given_pp_list = sort_pp_list(given_pp_list)
+    plays_added = 0
+
+    for pp in given_pp_list:
+        index = -1 - plays_added
+        if pp > pp_list[index]:
+            pp_list[index] = pp
+            plays_added += 1
+    pp_list = sort_pp_list(pp_list)
+    return pp_list, plays_added
+
+def get_ntp(osu_user, pp_tuple):
     get_best_scores = stuff.get_user_best1(osu_user)
     get_user = stuff.get_user(osu_user)
-    pp = []
     if not get_user:  return f"~~{osu_user}~~ **was not found.**"
     try:
-        bonus_pp = 0
-        for z in range(len(get_best_scores)):
-            bonus_pp += float(get_best_scores[z]['pp']) * (0.95 ** z)
-            pp.append(float(get_best_scores[z]['pp']))
-        now_pp = float(get_user[0]['pp_raw'])
-        bonus_pp = now_pp - bonus_pp
-        total_pp = bonus_pp
-        multi_given_pp = False
-        if len(given_pp) < 2:
-            given_pp = given_pp[0]
-            if given_pp > pp[-1]:
-                del pp[-1]
-                pp.append(given_pp)
-                pp.sort(reverse = True)
-                play_number = pp.index(given_pp) + 1
+        pp_list = get_list_of_pp(get_best_scores)
+        bonus_pp = calculate_total_pp(pp_list)
+
+        now_pp = float(get_user[0]['pp_raw']) # defalt raw pp
+        bonus_pp = now_pp - bonus_pp          # bonus_pp = default raw pp - default raw pp that i calculated (this works cause we cant calculate bonus pp)
+        total_pp = bonus_pp                   # total_pp has bonus_pp now imma do += to total pp now
+
+        more_than_one = False
+        if len(pp_tuple) < 2:
+            given_pp = float(pp_tuple[0])
+            if given_pp > pp_list[-1]:
+                pp_list[-1] = given_pp
+                pp_list = sort_pp_list(pp_list)
+                play_number = pp_list.index(given_pp) + 1
             else: return f"This play wont give **{get_user[0]['username']}** any pp"
         else:
-            good_variable = get_new_top_plays(given_pp, pp)
-            multi_given_pp = True
-            pp = good_variable[0]
-            if good_variable[1] < 1: return f"These plays wont give **{get_user[0]['username']}** any pp"
-        for z in range(len(pp)):
-            total_pp += pp[z] * (0.95 ** z)
+            return_value = get_ntps(pp_tuple, pp_list)
+            pp_list = return_value[0]
+            plays_added = return_value[1]
+
+            more_than_one = True
+            if plays_added < 1: return f"These plays wont give **{get_user[0]['username']}** any pp"
+        total_pp += calculate_total_pp(pp_list) # += because it has bonus_pp
         pp_increase = total_pp - now_pp
-        if not multi_given_pp:
+        if not more_than_one:
             return f"**{round(float(given_pp), 2)}**pp play would be **{get_user[0]['username']}**'s **#{play_number}** Personal Best. Total pp achieved would be **{round(total_pp, 2)}**pp. **(+{round(pp_increase, 2)})**"
         else:
-            return f"**{good_variable[1]}** plays added to **{get_user[0]['username']}**'s top plays  Total pp achieved would be **{round(total_pp,2)}**pp  **(+{round(pp_increase, 2)})**"
+            return f"**{plays_added}** plays added to **{get_user[0]['username']}**'s top plays  Total pp achieved would be **{round(total_pp,2)}**pp  **(+{round(pp_increase, 2)})**"
     except:
         return "Something went wrong!"
-
-        
-        
-def get_new_top_plays(given_pp_list, pp): # fixed
-    added_pp_list = []
-    plays_added = 0
-    given_pp_list = sorted(given_pp_list, reverse=True)
-    for item in given_pp_list:
-        if item > pp[99-plays_added]:
-            plays_added += 1
-            added_pp_list.append(item)
-    del pp[-len(added_pp_list):]
-    pp.extend(added_pp_list)
-    pp.sort(reverse = True)
-    return pp, plays_added
