4b64f6012f133d3a0883c52d2de9fce48d1ecf27
Kizuwe1lf <Kizuwe1lf@gmail.com>
2021-04-13 12:16:39 +0300

    PerformanceCalculator implementation

--- bot_commands/c_global.py
+++ bot_commands/c_global.py
@@ -6,11 +6,9 @@ from write_to_image import *
 def get_global_image(guild_id, mods):
     beatmap_id = stuff.get_beatmap_id(guild_id)
     if not beatmap_id: return f"Theres no ~~beatmap_id~~ in cache"
-    if mods == "":
-        pass
-    else:
-        mods = mod_to_num(mods)
-    scores = stuff.get_global(beatmap_id, mods)
+    if check_if_mods_are_invalid(mods) == 0:
+        return 'Invalid Mods'
+    mods_int = mod_to_num(mods)
+    scores = stuff.get_global(beatmap_id, mods_int)
     get_beatmaps = stuff.get_beatmaps(beatmap_id, guild_id)
     return write_to_image(scores, get_beatmaps)
-    
