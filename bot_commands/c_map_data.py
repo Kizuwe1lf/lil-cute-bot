0036ce99e7926e6b35d66910b76283b15a1af740
Kizuwe1lf <Kizuwe1lf@gmail.com>
2021-04-13 12:13:52 +0300

    changed >map command entirely now gives different unique information but lacks some basic info like cs hp ar but whatever thats good

--- bot_commands/c_map_data.py
+++ bot_commands/c_map_data.py
@@ -5,12 +5,19 @@ import discord
 import calendar
 
 
-def get_len(seconds):
+def get_len_and_bpm(seconds, bpm, mod):
+    if mod == 'DT':
+        seconds /= 1.5 # dt formula it increases the speed of beatmaps by 150%
+        bpm *= 1.5
+    elif mod == 'HT':
+        seconds /= 0.75 # ht formula it decreases the speed of beatmaps by 75%
+        bpm /= 0.75
+
     minutes = int(seconds / 60)
-    seconds = seconds - minutes * 60
+    seconds = round(seconds - minutes * 60)
     if seconds < 10:
         seconds = f"0{seconds}"
-    return [minutes, seconds]
+    return [minutes, seconds, round(bpm)]
 
 
 def get_beatmap_status(beatmap_status):
@@ -23,19 +30,16 @@ def get_beatmap_status(beatmap_status):
     if beatmap_status == -2: return "Graveyard"
 
 
-def spot_beatmap(guild_id, beatmap_id: str = None):
+def spot_beatmap(guild_id, beatmap_id, mods: list = ['No Mod']):
     if beatmap_id == None:
         beatmap_id = stuff.get_beatmap_id(guild_id)
         if not beatmap_id: return f"Theres no ~~beatmap_id~~ in cache"
     else:
         beatmap_id = beatmap_id.split("/")
         beatmap_id = beatmap_id[-1]
-    beatmap = Beatmaps(beatmap_id)
-    beatmap_data = beatmap.get_beatmap_data()
-    beatmap_with_dt = Beatmaps(beatmap_id, 64)
     get_beatmaps = stuff.get_beatmaps(beatmap_id, guild_id)
     get_user = stuff.get_user(get_beatmaps[0]['creator'])
-    r_g_b = download_thumbnails(get_beatmaps[0]['beatmapset_id'])
+    r_g_b = get_avg_colour_from_cover(get_beatmaps[0]['beatmapset_id'])
     e = discord.Embed(color=discord.colour.Colour.from_rgb(r_g_b[0], r_g_b[1], r_g_b[2]))
     if not get_user:
         e.set_author(name=f"Beatmap by {get_beatmaps[0]['creator']}")
@@ -44,17 +48,37 @@ def spot_beatmap(guild_id, beatmap_id: str = None):
         profile_url = f"https://osu.ppy.sh/u/{get_user[0]['user_id']}"
         e.set_author(name=f"Beatmap by {get_user[0]['username']}", url=profile_url, icon_url=avatar_url)
 
-    hit_len = get_len(int(get_beatmaps[0]['hit_length']))
-    total_len = get_len(int(get_beatmaps[0]['total_length']))
+    mods_list = get_mod_list_from_mods_string(mods)
+
+    if check_if_mods_are_invalid(mods_list) == 0:
+        return 'Invalid Mods'
+
     objectz = [int(get_beatmaps[0]['count_normal']), int(get_beatmaps[0]['count_slider']), int(get_beatmaps[0]['count_spinner']), ]
     obj = objectz[0] + objectz[1] + objectz[2]
+
+
+    star_rating = get_difficulty(beatmap_id, mods_list)
+    map_data = get_beatmap_data(beatmap_id, mods_list)
+    mods_string = ''.join(mods_list)
+
+    if 'DT' or 'NC' in mods_list:
+        mod_formula = 'DT'
+    elif 'HT' in mods_list:
+        mod_formula = 'HT'
+    else:
+        mod_formula = 'Wee'
+
+    bpm = float(get_beatmaps[0]['bpm'])
+    hit_len = get_len_and_bpm(int(get_beatmaps[0]['hit_length']), bpm, mod_formula)
+    total_len = get_len_and_bpm(int(get_beatmaps[0]['total_length']), bpm, mod_formula)
 
     info = ""
     info += f"**[{get_beatmaps[0]['artist']} - {get_beatmaps[0]['title'].replace('*', ' ')} \n [{get_beatmaps[0]['version']}]]"
     info += f"(https://osu.ppy.sh/beatmapsets/{get_beatmaps[0]['beatmapset_id']}#osu/{get_beatmaps[0]['beatmap_id']})**\n \n"
-    info += f"**Length:** `{total_len[0]}:{total_len[1]}` (`{hit_len[0]}:{hit_len[1]}`) **BPM:** `{get_beatmaps[0]['bpm']}` **Objects:** `{obj}`\n \n"
-    info += f"**Circles:** `{objectz[0]}` **Sliders:** `{objectz[1]}` **Spinners:** `{objectz[2]}`\n"
-    info += f"**CS:** `{beatmap_data[0]}` **AR:** `{beatmap_data[1]}` **OD:** `{beatmap_data[2]}` **HP:** `{beatmap_data[3]}` **SR:** `{beatmap.get_stars()}` (`{beatmap_with_dt.get_stars()}` DT) **MaxCombo:** `{get_beatmaps[0]['max_combo']}`\n \n"
+    info += f"**Mods** +{mods_string}\n\n"
+    info += f"**Length:** `{total_len[0]}:{total_len[1]}` (`{hit_len[0]}:{hit_len[1]}`) **BPM:** `{hit_len[2]}` **MaxCombo:** `{get_beatmaps[0]['max_combo']}`\n"
+    info += f"**Objects:**    | **Circles:** `{objectz[0]}` **Sliders:** `{objectz[1]}` **Spinners:** `{objectz[2]}` **Total** `{obj}`\n"
+    info += f"**StarRatings** | **Aim** `{map_data['aimsr']}` **Speed** `{map_data['speedsr']}` **Total** `{map_data['sr']}`\n"
 
     footer_text = ""
     footer_text += f"{get_beatmap_status(int(get_beatmaps[0]['approved']))} | {get_beatmaps[0]['favourite_count']} ❤"
