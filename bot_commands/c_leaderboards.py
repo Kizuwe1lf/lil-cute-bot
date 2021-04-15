c3f1d6af1b90e097db8c532a7ed1d7c81c277513
Kizuwe1lf <Kizuwe1lf@gmail.com>
2021-04-15 16:03:42 +0300

    now sending embed from inside command functions and new db implementation

--- bot_commands/c_leaderboards.py
+++ bot_commands/c_leaderboards.py
@@ -1,34 +1,26 @@
 import discord
-from database import *
 
 
 
-def get_leaderboards(ctx):
-    db_object = Database()
-    discord_id_list = []
+async def commands_leaderboards(ctx, db_obj):
+    members = db_obj.select_players_by_server(ctx.guild.id)
+    members = sorted(members, key=lambda x:x['pp_raw'], reverse=True)
+
     description = ""
-
-
-    for member in ctx.guild.members:
-        discord_id_list.append(member.id)
-
-    member_list = db_object.multiple_select_from_database(discord_id_list)
     multi_link_count = 0
-    member_list.sort(key=lambda x:x[4], reverse=true)
-
     i = 0
-    while i+multi_link_count < len(member_list) and i+multi_link_count < 21:
-        if i >= 1 and member_list[i][2] == member_list[i-1][2]:
+    while i < len(members) and i < 21:
+        if i >= 1 and members[i]['osu_user_id'] == members[i-1]['osu_user_id']:
             multi_link_count += 1
         else:
-            profile_url = f"(https://osu.ppy.sh/u/{member_list[i][2]})"
-            osu_user = f"[{member_list[i][1]}]"
-            description += f"**{(i + 1)-multi_link_count}.** {osu_user}{profile_url} **{member_list[i][4]}pp** #{member_list[i][3]} {member_list[i][6]}#{member_list[i][5]}\n"
+            profile_url = f"(https://osu.ppy.sh/u/{members[i]['osu_user_id']})"
+            osu_user = f"[{members[i]['osu_username']}]"
+            description += f"**{(i + 1)-multi_link_count}.** {osu_user}{profile_url} **{members[i]['pp_raw']}pp** #{members[i]['pp_rank']} {members[i]['country']}#{members[i]['pp_country_rank']}\n"
         i += 1
 
     e = discord.Embed(colour=ctx.message.author.colour)
     e.set_author(name=f"osu! Ranking for {ctx.guild.name}")
     e.set_thumbnail(url=ctx.guild.icon_url)
     e.description = description
-    e.set_footer(text=f"{len(member_list)-multi_link_count} player linked in {ctx.guild.name}")
-    return e
+    e.set_footer(text=f"{len(members)-multi_link_count} player linked in {ctx.guild.name}")
+    await send_embed(ctx, e)
