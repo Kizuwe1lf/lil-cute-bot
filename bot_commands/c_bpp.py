68e7e810b9eb043e0f0d79406954fb201b45c0aa
Kizuwe1lf <Kizuwe1lf@gmail.com>
2021-04-15 16:01:23 +0300

    now sending embed from inside command functions

--- bot_commands/c_bpp.py
+++ bot_commands/c_bpp.py
@@ -1,15 +1,22 @@
 from bot_commands.c_main import stuff
 
 
-def get_bpp(osu_user):
-    try:
-        get_best_scores = stuff.get_user_best1(osu_user)
-        get_user = stuff.get_user(osu_user)
-        bonus_pp = 0
-        for z in range(len(get_best_scores)):
-            bonus_pp += float(get_best_scores[z]['pp']) * (0.95 ** z)
-        now_pp = float(get_user[0]['pp_raw'])
-        bonus_pp = now_pp - bonus_pp
-        return f"**{get_user[0]['username']}** has **{round(float(bonus_pp), 2)}** bonus pp"
-    except:
-        return "Something went wrong!"
+async def commands_bpp(ctx, player):
+    if player == None:
+        await ctx.send('User Not Linked')
+        return 0
+
+    osu_username = player['osu_username']
+    get_best_scores = stuff.get_user_best1(osu_username)
+    get_user = stuff.get_user(osu_username)
+
+    if not get_user:
+        await ctx.send(f"~~{osu_username}~~ **was not found.**")
+        return 0
+
+    bonus_pp = 0
+    for play in range(len(get_best_scores)):
+        bonus_pp += float(get_best_scores[play]['pp']) * (0.95 ** play) # pp weight formula
+    now_pp = float(get_user[0]['pp_raw'])
+    bonus_pp = now_pp - bonus_pp
+    await ctx.send(f"**{get_user[0]['username']}** has **{round_func(bonus_pp)}** bonus pp")
