c4ff27084214d413a21775836c518fa83f2cabbd
Kizuwe1lf <Kizuwe1lf@gmail.com>
2021-04-15 16:04:30 +0300

    now sending embed from inside command functions and new db implementation

--- bot_commands/c_global.py
+++ bot_commands/c_global.py
@@ -2,13 +2,17 @@ from bot_commands.c_main import stuff
 from write_to_image import *
 
 
+async def commands_global(ctx, mods):
+    beatmap_id = stuff.get_beatmap_id(ctx.channel.id)
 
-def get_global_image(guild_id, mods):
-    beatmap_id = stuff.get_beatmap_id(guild_id)
-    if not beatmap_id: return f"Theres no ~~beatmap_id~~ in cache"
+    if not beatmap_id:
+        await ctx.send(f"Theres no ~~beatmap_id~~ in cache")
+        return 0
     if check_if_mods_are_invalid(mods) == 0:
-        return 'Invalid Mods'
+        await ctx.send('Invalid Mods')
+        return 0
+
     mods_int = mod_to_num(mods)
     scores = stuff.get_global(beatmap_id, mods_int)
-    get_beatmaps = stuff.get_beatmaps(beatmap_id, guild_id)
-    return write_to_image(scores, get_beatmaps)
+    get_beatmaps = stuff.get_beatmaps(beatmap_id, ctx.channel.id)
+    await write_to_image(ctx, scores, get_beatmaps)
