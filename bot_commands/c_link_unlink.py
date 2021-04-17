337bf32cc399ea798257c798db9064f3f6bf09cd
Kizuwe1lf <Kizuwe1lf@gmail.com>
2021-04-17 16:14:40 +0300

    now link adds that server in your servers array if its not in (the servers which recognizes you)

--- bot_commands/c_link_unlink.py
+++ bot_commands/c_link_unlink.py
@@ -2,21 +2,26 @@ from bot_commands.c_main import stuff
 
 
 async def commands_link(ctx, osu_username, db_obj):
-    check_if_linked = db_obj.select_players_by_id(ctx.message.author.id)
+    user_data_in_db = db_obj.select_players_by_id(ctx.message.author.id)
 
-    if check_if_linked != None:
-        await ctx.send("Already Linked!")
-        return 0
+    if user_data_in_db == None: # if none link it
+        get_user = stuff.get_user(osu_username)
 
-    get_user = stuff.get_user(osu_username)
+        if not get_user:
+            await ctx.send("User Not Found")
+            return 0
 
-    if not get_user:
-        await ctx.send("User Not Found")
-        return 0
+        user_data = db_obj.preparing_user_data_for_db_functions(get_user, ctx.message.author.id, [], ctx.guild.id)
+        db_obj.insert_data(user_data)
+        await ctx.send('Linked <:mpeepoHappy:701361814738305095>')
+    else: # if in db
+        if ctx.guild.id in user_data_in_db['servers']:
+            await ctx.send('Already Linked! <:ramPout:716183173318443018>')
+        else:
+            user_data_in_db['servers'] += [ctx.guild.id]
+            db_obj.update_data(user_data_in_db)
+            await ctx.send(f'Now recognizing you in {ctx.guild.name} too! <:shades:627100412851388426>')
 
-    user_data = db_obj.preparing_user_data_for_db_functions(get_user, ctx.message.author.id, [], ctx.guild.id)
-    output = db_obj.insert_data(user_data)
-    await ctx.send('Linked <:mpeepoHappy:701361814738305095>')
 
 async def commands_unlink(ctx, db_obj):
     db_obj.delete_data(ctx.message.author.id)
