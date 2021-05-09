f6bd5536382b482da81de95f57308c5f296c5b5e
Kizuwe1lf <Kizuwe1lf@gmail.com>
2021-05-09 17:07:04 +0300

    style

--- bot_commands/c_acc.py
+++ bot_commands/c_acc.py
@@ -8,14 +8,11 @@ async def commands_acc(ctx, request_obj, count50, count100, mods:str = 'No Mod')
 
     if not beatmap_id:
         await ctx.send(f"Theres no ~~beatmap_id~~ in cache")
-        return 0
-    if check_if_mods_are_invalid(mods_list) == 0:
+    elif check_if_mods_are_invalid(mods_list) == 0:
         await ctx.send('Invalid Mods')
-        return 0
-    if count50.isnumeric() == False or count100.isnumeric() == False:
+    elif count50.isnumeric() == False or count100.isnumeric() == False:
         await ctx.send("Invalid Count50&Count100")
-        return 0
-
-    count = [0, int(count50), int(count100)]
-    if_fc_pp_resp = get_if_fc_pp(beatmap_id, mods_list, count)
-    await ctx.send(f"{if_fc_pp_resp['accuracy']}% +{mods_string} FC: **{if_fc_pp_resp['pp']}**pp")
+    else:
+        count = [0, int(count50), int(count100)]
+        if_fc_pp_resp = get_if_fc_pp(beatmap_id, mods_list, count)
+        await ctx.send(f"{if_fc_pp_resp['accuracy']}% +{mods_string} FC: **{if_fc_pp_resp['pp']}**pp")
--- bot_commands/c_help.py
+++ bot_commands/c_help.py
@@ -1,140 +1,139 @@
 import discord
 
 async def commands_help(ctx, prefix, command_name, bot_user_name):
-    output = "```"
+    # ``` text ``` < this ` chars are for discord text format
 
     if command_name == 'acc':
-        output += "Shows pp value for last specified beatmap\n"
-        output += f"If you wanna change last specified beatmap simply use {prefix}map command\n"
-        output += "Example Usages\n\n"
-        output += f"{prefix}acc 0 35 hddt"
-        output += f"{prefix}acc 5 4"
+        output = "```Shows pp value for last specified beatmap\n" \
+        "If you wanna change last specified beatmap simply use {prefix}map command\n" \
+        "Example Usages\n\n" \
+        f"{prefix}acc 0 35 hddt" \
+        f"{prefix}acc 5 4```"
 
     elif command_name == 'link':
-        output += "Links your osu account to your discord account\n"
-        output += "Benefits are you dont have to type osu_username parameter it automatically detects\n"
-        output += "Theres special commands for linked players ex: cs, graph, leaderboards\n"
-        output += "Example Usages\n\n"
-        output += f"{prefix}link osu_username\n"
-        output += f"{prefix}link      description: if you wanna recognize by bot from different servers simply use this command on these servers"
+        output = "```Links your osu account to your discord account\n" \
+        "Benefits are you dont have to type osu_username parameter it automatically detects\n" \
+        "Theres special commands for linked players ex: cs, graph, leaderboards\n" \
+        "Example Usages\n\n" \
+        f"{prefix}link osu_username\n" \
+        f"{prefix}link      description: if you wanna recognize by bot from different servers simply use this command on these servers```"
 
     elif command_name == 'unlink':
-        output += "Unlinks your account\n"
-        output += "Example Usage\n\n"
-        output += f"{prefix}unlink"
+        output = "```Unlinks your account\n" \
+        "Example Usage\n\n" \
+        f"{prefix}unlink```"
 
     elif command_name == 'osu':
-        output += "Shows osu profile of given player\n"
-        output += "Example Usages\n\n"
-        output += f"{prefix}osu osu_username\n"
-        output += f"{prefix}osu osu_username1 osu_username2 osu_username3...\n\n"
-        output += "instead of osu_username u can mention linked users or you just dont give any username for self use\n"
-        output += f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}"
+        output = "```Shows osu profile of given player\n" \
+        "Example Usages\n\n" \
+        f"{prefix}osu osu_username\n" \
+        f"{prefix}osu osu_username1 osu_username2 osu_username3...\n\n" \
+        "instead of osu_username u can mention linked users or you just dont give any username for self use\n" \
+        f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}```"
 
     elif command_name == 'osutop':
-        output += "Shows top plays for given username\n"
-        output += "Example Usages\n\n"
-        output += f"{prefix}osutop osu_username\n"
-        output += f"{prefix}osutop osu_username p 15      description: shows 15th play for given user\n"
-        output += f"{prefix}osutop osu_username r         description: shows latest 3 top plays for given user\n\n"
-        output += "instead of osu_username u can mention linked users or you just dont give any username for self use\n"
-        output += f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}"
+        output = "```Shows top plays for given username\n" \
+        "Example Usages\n\n" \
+        f"{prefix}osutop osu_username\n" \
+        f"{prefix}osutop osu_username p 15      description: shows 15th play for given user\n" \
+        f"{prefix}osutop osu_username r         description: shows latest 3 top plays for given user\n\n" \
+        "instead of osu_username u can mention linked users or you just dont give any username for self use\n" \
+        f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}```"
 
     elif command_name == 'recent':
-        output += "Shows most recent score for given username\n"
-        output += "Example Usage\n"
-        output += f"{prefix}recent osu_username\n\n"
-        output += "instead of osu_username u can mention linked users or you just dont give any username for self use\n"
-        output += f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}"
+        output = "```Shows most recent score for given username\n" \
+        "Example Usage\n" \
+        f"{prefix}recent osu_username\n\n" \
+        "instead of osu_username u can mention linked users or you just dont give any username for self use\n" \
+        f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}```"
 
     elif command_name == 'compare':
-        output += "Shows scores of given username for the last specified beatmap\n"
-        output += f"If you wanna change last specified beatmap simply use {prefix}map command\n"
-        output += "Example Usage\n"
-        output += f"{prefix}compare osu_username\n\n"
-        output += "instead of osu_username u can mention linked users or you just dont give any username for self use\n"
-        output += f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}"
+        output = "```Shows scores of given username for the last specified beatmap\n" \
+        f"If you wanna change last specified beatmap simply use {prefix}map command\n" \
+        "Example Usage\n" \
+        f"{prefix}compare osu_username\n\n" \
+        "instead of osu_username u can mention linked users or you just dont give any username for self use\n" \
+        f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}```"
 
     elif command_name == 'compareserver':
-        output += "Shows best 8 scores made by linked players in the server for last specified beatmap\n"
-        output += f"If you wanna change last specified beatmap simply use {prefix}map command\n"
-        output += "Example Usage\n"
-        output += f"{prefix}compareserver"
+        output = "```Shows best 8 scores made by linked players in the server for last specified beatmap\n" \
+        f"If you wanna change last specified beatmap simply use {prefix}map command\n" \
+        "Example Usage\n" \
+        f"{prefix}compareserver```"
 
     elif command_name == 'global':
-        output += "Shows global best 8 scores for the last specified beatmap\n"
-        output += f"If you wanna change last specified beatmap simply use {prefix}map command\n"
-        output += "Example Usages\n\n"
-        output += f"{prefix}global\n"
-        output += f"{prefix}global hddt"
+        output = "```Shows global best 8 scores for the last specified beatmap\n" \
+        f"If you wanna change last specified beatmap simply use {prefix}map command\n" \
+        "Example Usages\n\n" \
+        f"{prefix}global\n" \
+        f"{prefix}global hddt```"
 
     elif command_name == 'ntp':
-        output += "Adds given pp value to given player's top plays and simulates their new overall pp\n"
-        output += "Example Usages\n\n"
-        output += f"{prefix}ntp osu_username 500\n"
-        output += f"{prefix}ntp osu_username 500 500 500 500\n\n"
-        output += "instead of osu_username u can mention linked users or you just dont give any username for self use\n"
-        output += f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}"
+        output = "```Adds given pp value to given player's top plays and simulates their new overall pp\n" \
+        "Example Usages\n\n" \
+        f"{prefix}ntp osu_username 500\n" \
+        f"{prefix}ntp osu_username 500 500 500 500\n\n" \
+        "instead of osu_username u can mention linked users or you just dont give any username for self use\n" \
+        f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}```"
 
     elif command_name == 'bpp':
-        output += "Shows bonus pp of given player's pp\n"
-        output += "Example Usages\n\n"
-        output += f"{prefix}bpp osu_username\n\n"
-        output += "instead of osu_username u can mention linked users or you just dont give any username for self use\n"
-        output += f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}"
+        output = "```Shows bonus pp of given player's pp\n" \
+        "Example Usages\n\n" \
+        f"{prefix}bpp osu_username\n\n" \
+        "instead of osu_username u can mention linked users or you just dont give any username for self use\n" \
+        f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}```"
 
     elif command_name == 'leaderboards':
-        output += "Shows stat leaderboards for server\n"
-        output += "Example Usage\n\n"
-        output += f"{prefix}leaderboards"
+        output = "```Shows stat leaderboards for server\n" \
+        "Example Usage\n\n" \
+        f"{prefix}leaderboards```"
 
     elif command_name == 'globalboards':
-        output += f"Shows stat leaderboards for everyone linked with {bot_user_name}\n"
-        output += f"Example Usage\n\n"
-        output += f"{prefix}globalboards"
+        output = f"```Shows stat leaderboards for everyone linked with {bot_user_name}\n" \
+        f"Example Usage\n\n" \
+        f"{prefix}globalboards```"
 
     elif command_name == "map":
-        output += f"Shows beatmap info\n"
-        output += f"Mostly used for changing last specified beatmap\n"
-        output += f"Example Usages\n\n"
-        output += f"{prefix}map beatmap_link\n"
-        output += f"{prefix}map             description: info for last specified beatmap\n"
-        output += f"{prefix}map 123456      description: 123456 are beatmap id\n"
+        output = "```Shows beatmap info\n" \
+        "Mostly used for changing last specified beatmap\n" \
+        "Example Usages\n\n" \
+        f"{prefix}map beatmap_link\n" \
+        f"{prefix}map             description: info for last specified beatmap\n" \
+        f"{prefix}map 123456      description: 123456 are beatmap id```"
 
     elif command_name == "roll":
-        output += "Random random random numbers! fun command\n"
-        output += "Example Usages\n\n"
-        output += f"{prefix}roll          description: rolls between 1-100\n"
-        output += f"{prefix}roll 150      description: rolls between 1-150\n"
-        output += f"{prefix}roll iq       description: best usage LOL"
+        output = "```Random random random numbers! fun command\n" \
+        "Example Usages\n\n" \
+        f"{prefix}roll          description: rolls between 1-100\n" \
+        f"{prefix}roll 150      description: rolls between 1-150\n" \
+        f"{prefix}roll iq       description: best usage LOL```"
 
     elif command_name == "aliases":
-        output += "Shows commands aliases\n"
-        output += "Example Usage\n\n"
-        output += f"{prefix}aliases"
+        output = "```Shows commands aliases\n" \
+        "Example Usage\n\n" \
+        f"{prefix}aliases```"
 
     elif command_name == "setprefix":
-        output += "Changes bot prefix for server administrator only\n"
-        output += "Example Usages\n\n"
-        output += f"{prefix}setprefix new_prefix"
+        output = "```Changes bot prefix for server administrator only\n" \
+        "Example Usages\n\n" \
+        f"{prefix}setprefix new_prefix```"
 
     elif command_name == "graph":
-        output += "Shows really cool line chart for given player\n"
-        output += "Only works for linked users\n"
-        output += "Example Usages\n\n"
-        output += f"{prefix}graph osu_username pp 7      description: Shows last 7 day's pp line chart\n"
-        output += f"{prefix}graph osu_username rank      description: Shows a rank line chart with all data we have\n\n"
-        output += "Description: Available line charts 'pp', 'rank', 'acc', 'pc', 'countryrank'\n"
-        output += "instead of osu_username u can mention linked users or you just dont give any username for self use\n"
-        output += f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}"
+        output = "```Shows really cool line chart for given player\n" \
+        "Only works for linked users\n" \
+        "Example Usages\n\n" \
+        f"{prefix}graph osu_username pp 7      description: Shows last 7 day's pp line chart\n" \
+        f"{prefix}graph osu_username rank      description: Shows a rank line chart with all data we have\n\n" \
+        "Description: Available line charts 'pp', 'rank', 'acc', 'pc', 'countryrank'\n" \
+        "instead of osu_username u can mention linked users or you just dont give any username for self use\n" \
+        f"Examples: {prefix}{command_name} @discord_user or {prefix}{command_name}```"
 
     elif command_name == "invite":
-        output += "Gives you a nice invite link\n"
-        output += "Example Usage\n\n"
-        output += f"{prefix}invite"
+        output = "```Gives you a nice invite link\n" \
+        "Example Usage\n\n" \
+        f"{prefix}invite```"
 
     else:
         return await ctx.send(f"Invalid Command Name Type {prefix}help for command list")
 
-    output += "```"
     await ctx.send(output)
