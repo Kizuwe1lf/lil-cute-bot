b0bd1c19b0786c48b60ca9886719885a60e05a6d
Kizuwe1lf <kizuwe1lf@gmail.com>
2021-05-29 16:07:16 +0300

    f

--- bot_commands/c_global.py
+++ bot_commands/c_global.py
@@ -3,14 +3,12 @@ from write_to_image import *
 
 async def commands_global(ctx, request_obj, mods):
     beatmap_id = request_obj.get_beatmap_id(ctx.channel.id)
-
     if not beatmap_id:
         await ctx.send(f"Theres no ~~beatmap_id~~ in cache")
         return 0
     if check_if_mods_are_invalid(mods) == 0:
         await ctx.send('Invalid Mods')
         return 0
-
     mods_int = ""
     if mods != 'No Mod':
         mods_int = f"&mods={mod_to_num(mods)}"
