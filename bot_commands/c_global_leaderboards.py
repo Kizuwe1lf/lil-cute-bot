eef37826cb416a8b880c67fb8e34a7f3009c1b6e
Kizuwe1lf <kizuwe1lf@gmail.com>
2021-07-01 23:59:54 +0300

    fix related to 94ee98e
    from now on i don't need to sort members inside functions
    
    it is efficient a lot also easier to manipulate

--- bot_commands/c_global_leaderboards.py
+++ bot_commands/c_global_leaderboards.py
@@ -3,23 +3,21 @@ from scripts import *
 
 async def commands_global_leaderboards(ctx, db_obj, bot_avatar_url, bot_user_name):
     everyone = db_obj.select_everyone()
-    everyone = sorted(everyone, key=lambda x:x['pp_raw'], reverse=True)
-
-    description = ""
-    multi_link_count = 0
     i = 0
-    while i < len(everyone) and i < 20:
-        if i >= 1 and everyone[i]['osu_user_id'] == everyone[i-1]['osu_user_id']:
-            multi_link_count += 1
-        else:
-            profile_url = f"(https://osu.ppy.sh/u/{everyone[i]['osu_user_id']})"
-            osu_user = f"[{everyone[i]['osu_username']}]"
-            description += f"**{(i + 1)-multi_link_count}.** {osu_user}{profile_url} **{round_func(everyone[i]['pp_raw'])}pp** #{everyone[i]['pp_rank']} {everyone[i]['country']}#{everyone[i]['pp_country_rank']}\n"
+    description = []
+    for member in everyone:
         i += 1
+        profile_url = f"(https://osu.ppy.sh/u/{member['osu_user_id']})"
+        osu_user = f"[{member['osu_username']}]"
+        output = f"**{(i)}.** {osu_user}{profile_url} \
+                   **{round_func(member['pp_raw'])}pp** \
+                   #{member['pp_rank']} {member['country']}\
+                   #{member['pp_country_rank']}"
+        description.append(output)
 
     e = discord.Embed(colour=ctx.message.author.colour)
     e.set_author(name=f"osu! Global Ranking for {bot_user_name}")
     e.set_thumbnail(url=bot_avatar_url)
-    e.description = description
-    e.set_footer(text=f"{len(everyone)-multi_link_count} players linked with {bot_user_name}")
+    e.description = '\n'.join(description[:20])
+    e.set_footer(text=f"{i} players linked with {bot_user_name}")
     await send_embed(ctx, e)
--- bot_commands/c_leaderboards.py
+++ bot_commands/c_leaderboards.py
@@ -3,23 +3,21 @@ from scripts import *
 
 async def commands_leaderboards(ctx, db_obj):
     members = db_obj.select_players_by_server(ctx.guild.id)
-    members = sorted(members, key=lambda x:x['pp_raw'], reverse=True)
-
-    description = ""
-    multi_link_count = 0
     i = 0
-    while i < len(members) and i < 20:
-        if i >= 1 and members[i]['osu_user_id'] == members[i-1]['osu_user_id']:
-            multi_link_count += 1
-        else:
-            profile_url = f"(https://osu.ppy.sh/u/{members[i]['osu_user_id']})"
-            osu_user = f"[{members[i]['osu_username']}]"
-            description += f"**{(i + 1)-multi_link_count}.** {osu_user}{profile_url} **{round_func(members[i]['pp_raw'])}pp** #{members[i]['pp_rank']} {members[i]['country']}#{members[i]['pp_country_rank']}\n"
+    description = []
+    for member in members:
         i += 1
+        profile_url = f"(https://osu.ppy.sh/u/{member['osu_user_id']})"
+        osu_user = f"[{member['osu_username']}]"
+        output = f"**{(i)}.** {osu_user}{profile_url} \
+                   **{round_func(member['pp_raw'])}pp** \
+                   #{member['pp_rank']} {member['country']}\
+                   #{member['pp_country_rank']}"
+        description.append(output)
 
     e = discord.Embed(colour=ctx.message.author.colour)
     e.set_author(name=f"osu! Ranking for {ctx.guild.name}")
     e.set_thumbnail(url=ctx.guild.icon_url)
-    e.description = description
-    e.set_footer(text=f"{len(members)-multi_link_count} players linked in {ctx.guild.name}")
+    e.description = '\n'.join(description[:20])
+    e.set_footer(text=f"{i} players linked in {ctx.guild.name}")
     await send_embed(ctx, e)
