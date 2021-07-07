ec5162210e842d00a880f1835c7ebc45e4157fbc
Kizuwe1lf <kizuwe1lf@gmail.com>
2021-07-08 00:10:54 +0300

    fix missing backslash

--- bot_commands/c_help.py
+++ bot_commands/c_help.py
@@ -5,7 +5,7 @@ async def commands_help(ctx, prefix, command_name, bot_user_name):
 
     if command_name == 'acc':
         output = "```Shows pp value for last specified beatmap\n" \
-        "Parameters : 100count, 50count, mods(optional)"
+        "Parameters : 100count, 50count, mods(optional)" \
         "If you wanna change last specified beatmap simply use {prefix}map command\n" \
         "Example Usages\n\n" \
         f"{prefix}acc 5 4\n" \
